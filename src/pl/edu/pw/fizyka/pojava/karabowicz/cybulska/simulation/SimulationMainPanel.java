8b4dad38b946eb9ae59c8ee362053834a66e030a
Mpoziomki <karabowiczmalgorzata@gmail.com>
2020-05-07 11:11:26 +0200

    coś nie tak

--- src/pl/edu/pw/fizyka/pojava/karabowicz/cybulska/simulation/SimulationMainPanel.java
+++ src/pl/edu/pw/fizyka/pojava/karabowicz/cybulska/simulation/SimulationMainPanel.java
@@ -1,7 +1,5 @@
 package pl.edu.pw.fizyka.pojava.karabowicz.cybulska.simulation;
 
-import pl.edu.pw.fizyka.pojava.karabowicz.cybulska.universe.CelestialBodyPosition;
-
 import javax.swing.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -14,9 +12,6 @@ public class SimulationMainPanel extends JPanel implements ActionListener
 {
     private Timer animationTimer;
     public static final int delay = 10; //ms
-
-    private CelestialBodyPosition orbitCentre;
-    
 
     public SimulationMainPanel()
     {
